9c4b7ba0014aceb0d969b64bda5119dae8ba5a5d
Jo Pi <ferrdo@gmail.com>
2021-01-27 13:10:19 +0000

    added basic funcionality

--- event_handler.py
+++ event_handler.py
@@ -1,4 +1,45 @@
-from replit import db
+import discord
 
-def create_event():
-  
+def create_event(zeus, mission):
+  embed = discord.Embed(title = 'Misia')
+  embed.add_field(name = 'Misia', value = mission, inline = False)
+  embed.add_field(name = 'Zeus', value = zeus, inline = False)
+  embed.add_field(name = 'Cas irl', value = 'none')
+  embed.add_field(name = 'Cas v hre', value = 'none')
+  embed.add_field(name = 'Popis', value = 'none', inline = False)
+  return embed
+
+def set_times(embed, time_irl, time_game):
+  new_embed = discord.Embed(title = embed.title)
+  for f in embed.fields:
+    if f.name == 'Cas irl':
+      f.value = time_irl
+
+    if f.name == 'Cas v hre':
+      f.value = time_game
+
+    new_embed.add_field(name = f.name, value = f.value, inline = f.inline)
+  return new_embed
+
+def set_desc(embed, desc):
+  if desc.startswith('http'):
+    desc = '[doc](%s)' % desc
+
+  new_embed = discord.Embed(title = embed.title)
+  for f in embed.fields:
+    if f.name == 'Popis':
+      f.value = desc
+
+    new_embed.add_field(name = f.name, value = f.value, inline = f.inline)
+  return new_embed
+
+def check_zeus(embed, user):
+  for f in embed.fields:
+    print(f.name)
+    if f.name == 'Zeus':
+      print(f.value)
+      print(user)
+      if f.value == user:
+        return True
+      else:
+        return False
--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import discord
 import os
 
 from keep_alive import keep_alive
+from event_handler import create_event, set_times, set_desc, check_zeus
 
 client = discord.Client()
-
-sad_words = ["kill"]
 
 @client.event
 async def on_ready():
@@ -19,7 +18,33 @@ async def on_message(message):
   msg = message.content
 
   if msg.startswith('!help'):
-    await message.channel.send('do you need help ?!')
+    await message.channel.send('''!create [nazov misie]
+    !set_times [cas irl] [cas v hre]''')
+
+  if msg.startswith('!create'):
+    mission = msg.replace('!create', '')
+    embed_msg = create_event(message.author.name, mission)
+    await message.channel.send(embed=embed_msg)
+
+  if msg.startswith('!set_times'):
+    params = msg.split(' ')
+    message = await client.get_channel(message.channel.id).fetch_message(params[1])
+    embed = message.embeds[0]
+    if not check_zeus(embed, message.author.name):
+      await message.channel.send("Nie si Zeus!")
+      return
+    new_embed = set_times(embed, params[2], params[3])
+    await message.edit(embed=new_embed)
+
+  if msg.startswith('!set_desc'):
+    params = msg.split(' ')
+    message = await client.get_channel(message.channel.id).fetch_message(params[1])
+    embed = message.embeds[0]
+    if not check_zeus(embed, message.author.name):
+      await message.channel.send("Nie si Zeus!")
+      return
+    new_embed = set_desc(embed, params[2])
+    await message.edit(embed=new_embed)
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
