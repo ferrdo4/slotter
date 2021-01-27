8e5a6ca33af5342e7e7d232e1cf508bddd407ecd
Jo Pi <ferrdo@gmail.com>
2021-01-27 15:37:23 +0000

    added check and help

--- main.py
+++ main.py
@@ -17,17 +17,17 @@ async def on_message(message):
 
   msg = message.content
 
-  if msg.startswith('!help'):
-    await message.channel.send('''!create [nazov misie]
-    !set_times [cas irl] [cas v hre]''')
-
   if msg.startswith('!create'):
     mission = msg.replace('!create', '')
     embed_msg = create_event(message.author.name, mission)
     await message.channel.send(embed=embed_msg)
+    return
 
   if msg.startswith('!set_times'):
     params = msg.split(' ')
+    if len(params) != 4:
+      await message.channel.send("use !set_times [message_id] [time irl] [time ingame]")
+      return
     old_message = await client.get_channel(message.channel.id).fetch_message(params[1])
     embed = old_message.embeds[0]
     if not check_zeus(embed, message.author.name):
@@ -35,19 +35,27 @@ async def on_message(message):
       return
     new_embed = set_times(embed, params[2], params[3])
     await old_message.edit(embed=new_embed)
+    return
 
   if msg.startswith('!set_desc'):
     params = msg.split(' ')
+    if len(params) < 3:
+      await message.channel.send("use !set_desc [message_id] [desc/url]")
+      return
     old_message = await client.get_channel(message.channel.id).fetch_message(params[1])
     embed = old_message.embeds[0]
     if not check_zeus(embed, message.author.name):
       await message.channel.send("Nie si Zeus!")
       return
-    new_embed = set_desc(embed, params[2])
+    new_embed = set_desc(embed, msg.replace('!set_desc ', '').replace('%s '%params[1], ''))
     await old_message.edit(embed=new_embed)
+    return
 
   if msg.startswith('!set_team'):
     params = msg.split(' ')
+    if len(params) != 5:
+      await message.channel.send("use !set_team [message_id] [team] [role,role] [inline(True/False)]")
+      return
     old_message = await client.get_channel(message.channel.id).fetch_message(params[1])
     embed = old_message.embeds[0]
     if not check_zeus(embed, message.author.name):
@@ -55,9 +63,13 @@ async def on_message(message):
       return
     new_embed = set_team(embed, params[2], params[3], params[4])
     await old_message.edit(embed=new_embed)
+    return
 
   if msg.startswith('!rm_team'):
     params = msg.split(' ')
+    if len(params) != 3:
+      await message.channel.send("use !rm_team [message_id] [team]")
+      return
     old_message = await client.get_channel(message.channel.id).fetch_message(params[1])
     embed = old_message.embeds[0]
     if not check_zeus(embed, message.author.name):
@@ -65,7 +77,7 @@ async def on_message(message):
       return
     new_embed = rm_team(embed, params[2])
     await old_message.edit(embed=new_embed)
-
+    return
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
